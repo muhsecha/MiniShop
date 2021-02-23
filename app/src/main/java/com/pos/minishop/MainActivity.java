1cbe475a0b0ebe7ac2d49a764cc9c15506cb56f2
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2021-02-23 15:46:42 +0700

    update logout

--- app/src/main/java/com/pos/minishop/MainActivity.java
+++ app/src/main/java/com/pos/minishop/MainActivity.java
@@ -1,21 +1,31 @@
 package com.pos.minishop;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.pos.minishop.R;
+import com.pos.minishop.baseUrl.BaseUrl;
 import com.pos.minishop.ui.home.HomeFragment;
 import com.pos.minishop.ui.logOut.LogOutFragment;
 import com.pos.minishop.ui.member.MemberFragment;
 import com.pos.minishop.ui.transaksi.TransFragment;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -26,6 +36,9 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -65,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 fragment = new TransFragment();
                 break;
             case R.id.nav_logout:
-                fragment = new LogOutFragment();
+                logout();
                 break;
             case R.id.nav_member:
                 fragment = new MemberFragment();
@@ -97,5 +110,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    public void logout() {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage("Apakah anda yakin ingin keluar ?")
+                .setNegativeButton("Tidak", null)
+                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        SharedPreferences sp = getSharedPreferences("login", MODE_PRIVATE);
+                        String token = sp.getString("logged", "missing");
 
+                        AndroidNetworking.post(BaseUrl.url + "api/logout")
+                                .addHeaders("Authorization", "Bearer " + token)
+                                .setPriority(Priority.MEDIUM)
+                                .build()
+                                .getAsJSONObject(new JSONObjectRequestListener() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            String status = response.getString("status");
+
+                                            if (status.equals("success")) {
+                                                SharedPreferences.Editor editor = sp.edit();
+                                                editor.clear();
+                                                editor.apply();
+
+                                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onError(ANError anError) {
+                                        Log.d("TAG", "onError: " + anError.getErrorDetail());
+                                        Log.d("TAG", "onError: " + anError.getErrorBody());
+                                        Log.d("TAG", "onError: " + anError.getErrorCode());
+                                        Log.d("TAG", "onError: " + anError.getResponse());
+                                    }
+                                });
+                    }
+                }).create().show();
+    }
 }
