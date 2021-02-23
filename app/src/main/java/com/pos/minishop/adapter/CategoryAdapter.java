8adb0f4645266323a95091f2e1f07cb6b1ad6553
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2021-02-23 23:32:27 +0700

    add alert dialog delete

--- app/src/main/java/com/pos/minishop/adapter/CategoryAdapter.java
+++ app/src/main/java/com/pos/minishop/adapter/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.pos.minishop.adapter;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.androidnetworking.AndroidNetworking;
@@ -58,39 +60,46 @@ public class CategoryAdapter  extends RecyclerView.Adapter<CategoryAdapter.Categ
         holder.btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sp = holder.itemView.getContext().getSharedPreferences("login", MODE_PRIVATE);
-                String token = sp.getString("logged", "missing");
+                new AlertDialog.Builder(holder.itemView.getContext())
+                        .setMessage("Delete ?")
+                        .setNegativeButton("No", null)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                SharedPreferences sp = holder.itemView.getContext().getSharedPreferences("login", MODE_PRIVATE);
+                                String token = sp.getString("logged", "missing");
 
-                AndroidNetworking.delete(BaseUrl.url + "api/product-categories/" + category.getId())
-                        .addHeaders("Authorization", "Bearer " + token)
-                        .setPriority(Priority.MEDIUM)
-                        .build()
-                        .getAsJSONObject(new JSONObjectRequestListener() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                try {
-                                    String status = response.getString("status");
+                                AndroidNetworking.delete(BaseUrl.url + "api/product-categories/" + category.getId())
+                                        .addHeaders("Authorization", "Bearer " + token)
+                                        .setPriority(Priority.MEDIUM)
+                                        .build()
+                                        .getAsJSONObject(new JSONObjectRequestListener() {
+                                            @Override
+                                            public void onResponse(JSONObject response) {
+                                                try {
+                                                    String status = response.getString("status");
 
-                                    if (status.equals("success")) {
-                                        String message = response.getString("message");
+                                                    if (status.equals("success")) {
+                                                        String message = response.getString("message");
 
-                                        Toast.makeText(holder.itemView.getContext(), message, Toast.LENGTH_SHORT).show();
-                                        categoryManagementActivity.getCategories();
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
+                                                        Toast.makeText(holder.itemView.getContext(), message, Toast.LENGTH_SHORT).show();
+                                                        categoryManagementActivity.getCategories();
+                                                    }
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onError(ANError anError) {
+                                                Toast.makeText(holder.itemView.getContext(), "error", Toast.LENGTH_SHORT).show();
+                                                Log.d("TAG", "onError: " + anError.getErrorDetail());
+                                                Log.d("TAG", "onError: " + anError.getErrorBody());
+                                                Log.d("TAG", "onError: " + anError.getErrorCode());
+                                                Log.d("TAG", "onError: " + anError.getResponse());
+                                            }
+                                        });
                             }
-
-                            @Override
-                            public void onError(ANError anError) {
-                                Toast.makeText(holder.itemView.getContext(), "error", Toast.LENGTH_SHORT).show();
-                                Log.d("TAG", "onError: " + anError.getErrorDetail());
-                                Log.d("TAG", "onError: " + anError.getErrorBody());
-                                Log.d("TAG", "onError: " + anError.getErrorCode());
-                                Log.d("TAG", "onError: " + anError.getResponse());
-                            }
-                        });
+                        }).create().show();
             }
         });
 
--- app/src/main/java/com/pos/minishop/adapter/DiscountAdapter.java
+++ app/src/main/java/com/pos/minishop/adapter/DiscountAdapter.java
@@ -1,6 +1,8 @@
 package com.pos.minishop.adapter;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.androidnetworking.AndroidNetworking;
@@ -18,6 +21,8 @@ import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.pos.minishop.DiscountManagementActivity;
+import com.pos.minishop.LoginActivity;
+import com.pos.minishop.MainActivity;
 import com.pos.minishop.R;
 import com.pos.minishop.baseUrl.BaseUrl;
 import com.pos.minishop.model.DiscountModel;
@@ -60,38 +65,45 @@ public class DiscountAdapter extends RecyclerView.Adapter<DiscountAdapter.Discou
         holder.btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sp = holder.itemView.getContext().getSharedPreferences("login", MODE_PRIVATE);
-                String token = sp.getString("logged", "missing");
+                new AlertDialog.Builder(holder.itemView.getContext())
+                        .setMessage("Delete ?")
+                        .setNegativeButton("No", null)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                SharedPreferences sp = holder.itemView.getContext().getSharedPreferences("login", MODE_PRIVATE);
+                                String token = sp.getString("logged", "missing");
 
-                AndroidNetworking.delete(BaseUrl.url + "api/discounts/" + discountModel.getId())
-                        .addHeaders("Authorization", "Bearer " + token)
-                        .setPriority(Priority.MEDIUM)
-                        .build()
-                        .getAsJSONObject(new JSONObjectRequestListener() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                try {
-                                    String status = response.getString("status");
-                                    String message = response.getString("message");
+                                AndroidNetworking.delete(BaseUrl.url + "api/discounts/" + discountModel.getId())
+                                        .addHeaders("Authorization", "Bearer " + token)
+                                        .setPriority(Priority.MEDIUM)
+                                        .build()
+                                        .getAsJSONObject(new JSONObjectRequestListener() {
+                                            @Override
+                                            public void onResponse(JSONObject response) {
+                                                try {
+                                                    String status = response.getString("status");
+                                                    String message = response.getString("message");
 
-                                    if (status.equals("success")) {
-                                        Toast.makeText(holder.itemView.getContext(), message, Toast.LENGTH_SHORT).show();
-                                        discountManagementActivity.getDiscounts();
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
+                                                    if (status.equals("success")) {
+                                                        Toast.makeText(holder.itemView.getContext(), message, Toast.LENGTH_SHORT).show();
+                                                        discountManagementActivity.getDiscounts();
+                                                    }
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onError(ANError anError) {
+                                                Toast.makeText(holder.itemView.getContext(), "error", Toast.LENGTH_SHORT).show();
+                                                Log.d("TAG", "onError: " + anError.getErrorDetail());
+                                                Log.d("TAG", "onError: " + anError.getErrorBody());
+                                                Log.d("TAG", "onError: " + anError.getErrorCode());
+                                                Log.d("TAG", "onError: " + anError.getResponse());
+                                            }
+                                        });
                             }
-
-                            @Override
-                            public void onError(ANError anError) {
-                                Toast.makeText(holder.itemView.getContext(), "error", Toast.LENGTH_SHORT).show();
-                                Log.d("TAG", "onError: " + anError.getErrorDetail());
-                                Log.d("TAG", "onError: " + anError.getErrorBody());
-                                Log.d("TAG", "onError: " + anError.getErrorCode());
-                                Log.d("TAG", "onError: " + anError.getResponse());
-                            }
-                        });
+                        }).create().show();
             }
         });
 
