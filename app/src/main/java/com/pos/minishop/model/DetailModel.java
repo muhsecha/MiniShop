cbac21ea8a51e5838e359dde40c4161c9fbeabea
Reyhz <reyhanhammamz@gmail.com>
2021-02-27 11:05:27 +0700

    add detail list

---
+++ app/src/main/java/com/pos/minishop/model/DetailModel.java
@@ -0,0 +1,53 @@
+package com.pos.minishop.model;
+
+public class DetailModel {
+    String nama,stock,harga,jam,tanggal,total;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getJam() {
+        return jam;
+    }
+
+    public void setJam(String jam) {
+        this.jam = jam;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+}
