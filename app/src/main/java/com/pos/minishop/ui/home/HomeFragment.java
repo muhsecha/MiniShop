83781c1c44285a9fff23d516bb6773d080c85652
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2021-03-02 11:11:15 +0700

    add intent to history

--- app/src/main/java/com/pos/minishop/ui/home/HomeFragment.java
+++ app/src/main/java/com/pos/minishop/ui/home/HomeFragment.java
@@ -6,29 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.pos.minishop.CategoryManagementActivity;
 import com.pos.minishop.DiscountManagementActivity;
+import com.pos.minishop.HistoryTransActivity;
 import com.pos.minishop.ProductManagementActivity;
 import com.pos.minishop.R;
 
 public class HomeFragment extends Fragment {
 
-    LinearLayout lnP,lnC,lnD;
+    LinearLayout lnP, lnC, lnD, lnH;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         lnP = root.findViewById(R.id.ln_product);
         lnC = root.findViewById(R.id.ln_category);
         lnD = root.findViewById(R.id.ln_discount);
+        lnH = root.findViewById(R.id.ln_history);
 
         lnP.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +44,13 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), DiscountManagementActivity.class));
+            }
+        });
+
+        lnH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), HistoryTransActivity.class));
             }
         });
 
