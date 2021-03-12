9c999a906aa97ecac1cc5f5789d792c595b8364a
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2021-03-12 10:01:14 +0700

    update base url

--- app/src/main/java/com/pos/minishop/baseUrl/BaseUrl.java
+++ app/src/main/java/com/pos/minishop/baseUrl/BaseUrl.java
@@ -1,5 +1,5 @@
 package com.pos.minishop.baseUrl;
 
 public class BaseUrl {
-    public static String url = "https://6dfaae8d2ac2.ngrok.io/";
+    public static String url = "https://7e211cbb8055.ngrok.io/";
 }
